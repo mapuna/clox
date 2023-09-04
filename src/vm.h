51547df960404e73f47410d3d707ddb32c653946
Anupam Gupta <mapuna_@outlook.com>
2023-09-04 11:50:54 +0530

    basic VM, stack, stack trace, arithmetic

---
+++ src/vm.h
@@ -0,0 +1,30 @@
+#ifndef CLOX_VM_H
+#define CLOX_VM_H
+
+#include "chunk.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk *chunk;
+    uint8_t *ip; /* Instruction pointer */
+    Value stack[STACK_MAX];
+    Value *stackTop;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+void initVM();
+void freeVM();
+
+/* The VM runs the chunk and then responds with a value from `InterpretResult`
+ * enum: */
+InterpretResult interpret(Chunk *chunk);
+void push(Value value);
+Value pop();
+
+#endif // CLOX_VM_H
