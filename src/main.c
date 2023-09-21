6d4bf2004aef1aa54a2eb9fa4ec4e5d14aa572fd
Anupam Gupta <mapuna_@outlook.com>
2023-09-21 11:54:43 +0530

    VM implementation deferred.

--- src/main.c
+++ src/main.c
@@ -29,6 +29,7 @@ int main(int argc, char *argv[]) {
 
     writeChunk(&chunk, OP_RETURN, 123);
 
+    disassembleChunk(&chunk, "test chunk");
     interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
