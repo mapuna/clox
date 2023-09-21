d5148d1a32fdcd4b717ac9ef30fad427854cad27
Anupam Gupta <mapuna_@outlook.com>
2023-09-21 18:12:51 +0530

    Finished token scanner.

--- src/vm.h
+++ src/vm.h
@@ -23,7 +23,8 @@ void freeVM();
 
 /* The VM runs the chunk and then responds with a value from `InterpretResult`
  * enum: */
-InterpretResult interpret(Chunk *chunk);
+// InterpretResult interpret(Chunk *chunk);
+InterpretResult interpret(const char *source);
 void push(Value value);
 Value pop();
 
