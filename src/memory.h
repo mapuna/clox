2197a9845028abf01c78e8c52385231e19245f26
Anupam Gupta <mapuna_@outlook.com>
2023-09-25 10:45:03 +0530

    Completed string constants handling

--- src/memory.h
+++ src/memory.h
@@ -2,6 +2,7 @@
 #define CLOX_MEMORY_H
 
 #include "common.h"
+#include "object.h"
 
 #define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity)*2)
 #define GROW_ARRAY(type, pointer, oldCount, newCount)                          \
@@ -10,6 +11,13 @@
 #define FREE_ARRAY(type, pointer, oldCount)                                    \
     reallocate(pointer, sizeof(type) * (oldCount), 0)
 
+#define ALLOCATE(type, count)                                                  \
+    (type *)reallocate(NULL, 0, sizeof(type) * (count))
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+
 void *reallocate(void *pointer, size_t oldSize, size_t newSize);
+
+void freeObjects();
 
 #endif // CLOX_MEMORY_H
