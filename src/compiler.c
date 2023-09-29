9b0657bf87a3f813ef0bde028c270708ea9b56eb
Anupam Gupta <mapuna_@outlook.com>
2023-09-29 11:22:36 +0530

    ERROR! Revisit global var assignment.

--- src/compiler.c
+++ src/compiler.c
@@ -25,7 +25,7 @@ typedef enum {
     PREC_PRIMARY
 } Precedence;
 
-typedef void (*ParseFn)();
+typedef void (*ParseFn)(bool canAssign);
 
 typedef struct {
     ParseFn prefix;
@@ -127,7 +127,7 @@ static void emitConstant(Value value) {
     emitBytes(OP_CONSTANT, makeConstant(value));
 }
 
-static void number() {
+static void number(bool canAssign) {
     double value = strtod(parser.previous.start, NULL);
     emitConstant(NUMBER_VAL(value));
 }
@@ -150,24 +150,29 @@ static void parsePrecedence(Precedence precedence) {
         return;
     }
 
-    prefixRule();
+    bool canAssign = precedence <= PREC_ASSIGNMENT;
+    prefixRule(canAssign);
 
     while (precedence <= getRule(parser.current.type)->precedence) {
         advance();
         ParseFn infixRule = getRule(parser.previous.type)->infix;
-        infixRule();
+        infixRule(canAssign);
+    }
+
+    if (canAssign && match(TOKEN_EQUAL)) {
+        error("Invalid assignment target");
     }
 }
 
 static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
 
-static void grouping() {
+static void grouping(bool canAssign) {
     expression();
     // Assuming initial '(' has already been consumed.
     consume(TOKEN_RIGHT_PAREN, "Expect ')' after expressions.");
 }
 
-static void unary() {
+static void unary(bool canAssign) {
     // Leading token for unary expressions is in "previous" -- so already
     // consumed.
     TokenType operatorType = parser.previous.type;
@@ -182,7 +187,7 @@ static void unary() {
     }
 }
 
-static void binary() {
+static void binary(bool canAssign) {
     TokenType operatorType = parser.previous.type;
     ParseRule *rule = getRule(operatorType);
     parsePrecedence((Precedence)(rule->precedence + 1));
@@ -203,7 +208,7 @@ static void binary() {
     }
 }
 
-static void literal() {
+static void literal(bool canAssign) {
     switch (parser.previous.type) {
         case TOKEN_FALSE: emitByte(OP_FALSE); break;
         case TOKEN_NIL: emitByte(OP_NIL); break;
@@ -212,17 +217,27 @@ static void literal() {
     }
 }
 
-static void string() {
+static void string(bool canAssign) {
     emitConstant(OBJ_VAL(
         copyString(parser.previous.start + 1, parser.previous.length - 2)));
 }
 
-static void namedVariable(Token name) {
+static void namedVariable(Token name, bool canAssign) {
     uint8_t arg = identifierConstant(&name);
+
+    if (canAssign && (TOKEN_EQUAL)) {
+        expression();
+        emitBytes(OP_SET_GLOBAL, arg);
+    } else {
+        emitBytes(OP_GET_GLOBAL, arg);
+    }
+
     emitBytes(OP_GET_GLOBAL, arg);
 }
 
-static void variable() { namedVariable(parser.previous); }
+static void variable(bool canAssign) {
+    namedVariable(parser.previous, canAssign);
+}
 
 ParseRule rules[] = {
     [TOKEN_LEFT_PAREN] = {grouping, NULL, PREC_NONE},
@@ -339,6 +354,7 @@ static void declaration() {
     } else {
         statement();
     }
+
     if (parser.panicMode)
         synchronize();
 }
